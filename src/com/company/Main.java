d7c9ca24fdc28b1569d83a6849eff19eba28b596
LastSamurai2 <44116639+LastSamurai2@users.noreply.github.com>
2022-02-27 10:37:50 +0100

    zadanie 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Animal dog = new Animal("canis","Szarik");
+
+        System.out.println("pies nazywa sie " + dog.name);
+        System.out.println("pies waży " + dog.getWeight());
+        dog.feed();
+        dog.feed();
+        dog.feed();
+        System.out.println("pies waży " + dog.getWeight());
+
 
     }
 }
